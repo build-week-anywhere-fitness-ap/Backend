43991468c9bdd8b019dbfbf833cda3b7a4dc3e81
steelworx <eileencuevas62@gmail.com>
2019-08-27 12:22:06 -0400

    fixed login/registration, added GET users endpoint

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -9,25 +9,31 @@ const router = express.Router();
 router.post('/register/', async (req, res) => {
     const userInfo = req.body;
 
-    if (userInfo.password && userInfo.userName) {
-        if (userInfo.firstName && userInfo.lastName) {
-            userInfo.password = bcrypt.hashSync(userInfo.password, 15);
-            try {
-                const id = await helper.register(userInfo);
-                res.status(201).json(id);
-            } catch (err) {
+    if (userInfo.password) {
+        if (userInfo.username) {
+            if (userInfo.firstName && userInfo.lastName) {
+                userInfo.password = bcrypt.hashSync(userInfo.password, 16);
+                try {
+                    const id = await helper.register(userInfo);
+                    res.status(201).json(id);
+                } catch (err) {
+                    res.status(403).json({
+                        error: `Couldn't register this user. User may already exist.`
+                    });
+                } //  attempt to register
+            } else {
                 res.status(403).json({
-                    error: `Couldn't register this user. User may already exist.`
+                    error: `Please include both first name and last name.`
                 });
-            } //  attempt to register
+            } // 'did they include names?' check
         } else {
             res.status(403).json({
-                error: `Please include both first name and last name.`
+                error: `Please include a password.`
             });
-        } // 'did they include names?' check
+        } // 'did they include a username?' check
     } else {
         res.status(403).json({
-            error: `Please include a username and password.`
+            error: `Please include a username.`
         });
     } // 'did they include a username and password?' check
 });
@@ -36,32 +42,52 @@ router.post('/register/', async (req, res) => {
 router.post('/login/', async (req, res) => {
     const { username, password } = req.body;
 
-    if (username && password) {
-        try {
-            let foundUser = await helper.loginStart(username);
+    if (username) {
+        if (password) {
+            try {
+                let response = await helper.loginStart(username);
 
-            if (bcrypt.compareSync(password, foundUser.password)) {
-                const token = helper.generateToken(foundUser);
-                res.status(200).json({
-                    id: foundUser.id,
-                    firstName: foundUser.firstName,
-                    lastName: foundUser.lastName,
-                    instructor: foundUser.instructor,
-                    client: foundUser.client,
-                    token: token
-                });
-            } else {
+                if (bcrypt.compareSync(password, response.password)) {
+                    const token = helper.generateToken(response);
+                    res.status(200).json({
+                        id: response.id,
+                        firstName: response.firstName,
+                        lastName: response.lastName,
+                        instructor: response.instructor,
+                        client: response.client,
+                        token: token
+                    });
+                } else {
+                    res.status(401).json({
+                        message: `Invalid username or password.`
+                    });
+                } // 'does password match?' check
+            } catch (error) {
                 res.status(401).json({
-                    message: `Invalid username or password.`
+                    message: `Invalid credentials.`
                 });
-            } // 'does password match?' check
-        } catch (error) {
-            res.status(401).json({ message: `Invalid username or password.` });
-        } // 'does user exist?' check
+            } // 'does user exist?' check
+        } else {
+            res.status(403).json({
+                error: `Please include a password.`
+            });
+        } // 'does password exists?' check
     } else {
         res.status(403).json({
-            error: `Please include both username and password.`
+            error: `Please include a username.`
         });
-    } // 'does username & password exists?' check
+    } // 'does username exists?' check
+});
+
+router.get('/users/', async (req, res) => {
+    try {
+        let users = await helper.getUsers();
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({
+            error: `Couldn't retrieve users at this time.`
+        });
+    }
 });
 module.exports = router;
