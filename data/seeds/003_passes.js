89d4918b4ce72fa1ad900f71fce60aa01addf8e1
steelworx <eileencuevas62@gmail.com>
2019-08-28 16:14:14 -0400

    created seed data for passes

---
+++ data/seeds/003_passes.js
@@ -0,0 +1,28 @@
+exports.seed = function(knex) {
+    // Deletes ALL existing entries
+    return knex('passes')
+        .truncate()
+        .then(function() {
+            // Inserts seed entries
+            return knex('passes').insert([
+                {
+                    client_id: 2,
+                    class_id: 2,
+                    timesUsed: 0,
+                    completed: false
+                },
+                {
+                    client_id: 1,
+                    class_id: 1,
+                    timesUsed: 3,
+                    completed: false
+                },
+                {
+                    client_id: 3,
+                    class_id: 3,
+                    timesUsed: 9,
+                    completed: false
+                }
+            ]);
+        });
+};
