e20ccd5c05b0b5d50bc7b8aedd33ce0b47ef0730
steelworx <eileencuevas62@gmail.com>
2019-08-29 13:51:52 -0400

    moved all seed cleanup to another seed file

--- data/seeds/002_classes.js
+++ data/seeds/002_classes.js
@@ -1,27 +1,23 @@
 exports.seed = function(knex) {
     // Deletes ALL existing entries
-    return knex('classes')
-        .del()
-        .then(function() {
-            return knex('classes').insert([
-                {
-                    name: 'Axe Practice',
-                    type: 'High Intensity',
-                    location: 'Garreg Mach Monastery',
-                    instructor_id: 4
-                },
-                {
-                    name: 'Riding Practice',
-                    type: 'High Intensity',
-                    location: 'Garreg Mach Monastery',
-                    instructor_id: 4
-                },
-                {
-                    name: 'Archery',
-                    type: 'High Intensity',
-                    location: 'Garreg Mach Monastery',
-                    instructor_id: 4
-                }
-            ]);
-        });
+    return knex('classes').insert([
+        {
+            name: 'Axe Practice',
+            type: 'High Intensity',
+            location: 'Garreg Mach Monastery',
+            instructor_id: 4
+        },
+        {
+            name: 'Riding Practice',
+            type: 'High Intensity',
+            location: 'Garreg Mach Monastery',
+            instructor_id: 4
+        },
+        {
+            name: 'Archery',
+            type: 'High Intensity',
+            location: 'Garreg Mach Monastery',
+            instructor_id: 4
+        }
+    ]);
 };
--- data/seeds/003_passes.js
+++ data/seeds/003_passes.js
@@ -1,28 +1,23 @@
 exports.seed = function(knex) {
     // Deletes ALL existing entries
-    return knex('passes')
-        .del()
-        .then(function() {
-            // Inserts seed entries
-            return knex('passes').insert([
-                {
-                    client_id: 2,
-                    class_id: 2,
-                    timesUsed: 0,
-                    completed: false
-                },
-                {
-                    client_id: 1,
-                    class_id: 1,
-                    timesUsed: 3,
-                    completed: false
-                },
-                {
-                    client_id: 3,
-                    class_id: 3,
-                    timesUsed: 9,
-                    completed: false
-                }
-            ]);
-        });
+    return knex('passes').insert([
+        {
+            client_id: 2,
+            class_id: 2,
+            timesUsed: 0,
+            completed: false
+        },
+        {
+            client_id: 1,
+            class_id: 1,
+            timesUsed: 3,
+            completed: false
+        },
+        {
+            client_id: 3,
+            class_id: 3,
+            timesUsed: 9,
+            completed: false
+        }
+    ]);
 };
